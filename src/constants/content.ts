9c9730c58ca7ccd465d6fb8bd27519d6a9fbcc88
vinay096 <vinaysinghyadav06@gmail.com>
2025-05-04 17:12:29 +0530

    created content file

---
+++ src/constants/content.ts
@@ -0,0 +1,28 @@
+export const CONTENT = {
+  ContactFormPage: { Owner: "Owner", SendMessage: "Send a message about" },
+  ContactUSPage: {
+    BusinessHours: "Business Hours",
+    CallUs: "Call Us",
+    ContactAssistant:
+      "We're here to help you find your perfect home. Reach out to our team for personalized assistance.",
+    ContactUs: "Contact Us",
+    CustomerSupport: "Customer Support: +91 98765 43210",
+    DelhiAddress:
+      "42 Sardar Patel Road, Connaught Place, New Delhi, 110001, India",
+    EmailAddress: "Email Address*",
+    EmailUs: "Email Us",
+    FullName: "Full Name*",
+    GeneralInquiries: " General Inquiries: contact@homeyatra.in",
+    MonSat: "Monday - Saturday: 9AM - 7PM",
+    OurLocation: "Our Location",
+    PhoneNumber: "Phone Number*",
+    ReceivedInquiry:"We have received your inquiry and will get back to you soon.",
+    SalesInquiries: "Sales Inquiries: +91 98765 43211",
+    SelectTopic: "Select a topic*",
+    SendUs:"Send Us a Message",
+    Subject: "Subject*",
+    SundayTime: "Sunday: 10AM - 2PM (Online Support Only)",
+    SupportEmail: "Support: support@homeyatra.in",
+    ThankMessage:"Thank you for your message!",
+  },
+};
