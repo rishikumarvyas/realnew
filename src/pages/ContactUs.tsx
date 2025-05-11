d9f696b6e015e086110cc5398ab30b0e604621bb
subhash <yadav.subhash1688@gmail.com>
2025-05-11 13:59:03 +0530

    work tracker

--- src/pages/ContactUs.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ContactUs as ContactUsComponent } from "../components/ContactUs";
-
-const ContactUs: React.FC = () => {
-  return <ContactUsComponent />;
-};
-
-export default ContactUs;
