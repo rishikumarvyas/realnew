8dfd9e59289a4148fdcb4864eaac3a230125c7b8
vinay096 <vinaysinghyadav06@gmail.com>
2025-07-15 22:36:17 +0530

    formatted with prettier

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,28 +9,15 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import {
-  Menu,
-  X,
-  User,
-  LogOut,
-  PlusCircle,
-  Home,
-  Shield,
-} from "lucide-react";
+import { Menu, X, User, LogOut, PlusCircle, Home, Shield } from "lucide-react";
 import { useAuth } from "@/contexts/AuthContext";
 import { cn } from "@/lib/utils";
 import hy from "../Images/hy1-removebg-preview.png";
 
 export function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const {
-    user,
-    isAuthenticated,
-    logout,
-    openLoginModal,
-    openSignupModal,
-  } = useAuth();
+  const { user, isAuthenticated, logout, openLoginModal, openSignupModal } =
+    useAuth();
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -89,7 +76,9 @@ export function Navbar() {
                 onMouseOver={(e) =>
                   (e.currentTarget.style.transform = "scale(1.05)")
                 }
-                onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
+                onMouseOut={(e) =>
+                  (e.currentTarget.style.transform = "scale(1)")
+                }
               />
             </Link>
             <div className="hidden sm:ml-8 sm:flex sm:space-x-6">
@@ -120,74 +109,73 @@ export function Navbar() {
               >
                 Contact
               </Link>
-              
             </div>
           </div>
 
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
             {isAuthenticated ? (
               <>
-              {user?.role === 'Admin' && (
-                <Button
-                  variant="outline"
-                  onClick={() => navigate('/admin')}
-                  className="flex items-center gap-1 bg-blue-600 text-white hover:bg-white-700 rounded-full mr-2"
-                >
-                  <Shield className="h-4 w-4 mr-1" />
-                  Admin
-                </Button>
-              )}
-              <div className="flex items-center space-x-4">
-                <Button
-                  variant="outline"
-                  onClick={handlePostPropertyClick}
-                  className="flex items-center gap-1 bg-blue-600 text-white hover:bg-white-700 rounded-full"
-                >
-                  <PlusCircle className="h-4 w-4 mr-1" />
-                  Post Property
-                </Button>
-                <NotificationIcon />
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button
-                      variant="ghost"
-                      className="relative h-10 w-10 rounded-full bg-blue-100 text-blue-600 hover:bg-blue-200"
-                    >
-                      <User className="h-5 w-5" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent
-                    align="end"
-                    className="w-56 p-2 border border-blue-100"
+                {user?.role === "Admin" && (
+                  <Button
+                    variant="outline"
+                    onClick={() => navigate("/admin")}
+                    className="flex items-center gap-1 bg-blue-600 text-white hover:bg-white-700 rounded-full mr-2"
                   >
-                    <div className="px-2 py-2 font-medium text-gray-700">
-                      <div className="flex items-center gap-2">
-                        <User className="h-4 w-4 text-blue-500" />
-                        {user?.name}
-                      </div>
-                    </div>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem asChild className="hover:bg-blue-50">
-                      <Link
-                        to="/dashboard"
-                        className="cursor-pointer flex items-center py-1"
+                    <Shield className="h-4 w-4 mr-1" />
+                    Admin
+                  </Button>
+                )}
+                <div className="flex items-center space-x-4">
+                  <Button
+                    variant="outline"
+                    onClick={handlePostPropertyClick}
+                    className="flex items-center gap-1 bg-blue-600 text-white hover:bg-white-700 rounded-full"
+                  >
+                    <PlusCircle className="h-4 w-4 mr-1" />
+                    Post Property
+                  </Button>
+                  <NotificationIcon />
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="relative h-10 w-10 rounded-full bg-blue-100 text-blue-600 hover:bg-blue-200"
                       >
-                        <Home className="mr-2 h-4 w-4 text-blue-500" />
-                        Dashboard
-                      </Link>
-                    </DropdownMenuItem>
-
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem
-                      onClick={handleLogout}
-                      className="cursor-pointer hover:bg-red-50 flex items-center py-1"
+                        <User className="h-5 w-5" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                      align="end"
+                      className="w-56 p-2 border border-blue-100"
                     >
-                      <LogOut className="mr-2 h-4 w-4 text-red-500" />
-                      <span className="text-red-500">Logout</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </div>
+                      <div className="px-2 py-2 font-medium text-gray-700">
+                        <div className="flex items-center gap-2">
+                          <User className="h-4 w-4 text-blue-500" />
+                          {user?.name}
+                        </div>
+                      </div>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem asChild className="hover:bg-blue-50">
+                        <Link
+                          to="/dashboard"
+                          className="cursor-pointer flex items-center py-1"
+                        >
+                          <Home className="mr-2 h-4 w-4 text-blue-500" />
+                          Dashboard
+                        </Link>
+                      </DropdownMenuItem>
+
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem
+                        onClick={handleLogout}
+                        className="cursor-pointer hover:bg-red-50 flex items-center py-1"
+                      >
+                        <LogOut className="mr-2 h-4 w-4 text-red-500" />
+                        <span className="text-red-500">Logout</span>
+                      </DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
               </>
             ) : (
               <div className="flex items-center space-x-3">
@@ -214,7 +202,11 @@ export function Navbar() {
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-blue-600"
             >
               <span className="sr-only">Open main menu</span>
-              {isOpen ? <X className="block h-6 w-6" /> : <Menu className="block h-6 w-6" />}
+              {isOpen ? (
+                <X className="block h-6 w-6" />
+              ) : (
+                <Menu className="block h-6 w-6" />
+              )}
             </button>
           </div>
         </div>
@@ -263,11 +255,11 @@ export function Navbar() {
                 <Home className="h-4 w-4 mr-1" />
                 Dashboard
               </Link>
-              {user?.role === 'Admin' && (
+              {user?.role === "Admin" && (
                 <Button
                   variant="outline"
                   onClick={() => {
-                    navigate('/admin');
+                    navigate("/admin");
                     setIsOpen(false);
                   }}
                   className="w-full justify-center flex items-center gap-1 bg-blue-600 text-white hover:bg-white-700 rounded-full my-2"
